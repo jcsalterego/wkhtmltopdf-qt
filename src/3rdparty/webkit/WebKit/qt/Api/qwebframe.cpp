05a82d24ad3be989f70f4a93ccc2a2c21c54a003
Antialize <antialize@gmail.com>
2009-09-24 10:56:44 +0200

    Hack to allow one to print multiple pages into one document

--- src/3rdparty/webkit/WebKit/qt/Api/qwebframe.cpp
+++ src/3rdparty/webkit/WebKit/qt/Api/qwebframe.cpp
@@ -197,17 +197,11 @@ QString QWEBKIT_EXPORT qt_drt_counterValueForElementById(QWebFrame* qFrame, cons
 }
 
 #ifndef QT_NO_PRINTER
-
-QWebPrinterBeginCaller::QWebPrinterBeginCaller(QPainter &painter, QPrinter *printer)
-{
-    painter.begin(printer);
-}
-
-QWebPrinterPrivate::QWebPrinterPrivate(const QWebFrame *f, QPrinter *printer)
+QWebPrinterPrivate::QWebPrinterPrivate(const QWebFrame *f, QPrinter *printer, QPainter &p)
     : printContext(f->d->frame)
-    , beginCaller(painter, printer)
+    , painter(p)
     , frame(f)
-    , graphicsContext(&painter)
+    , graphicsContext(&p)
 {
     const qreal zoomFactorX = printer->logicalDpiX() / qt_defaultDpi();
     const qreal zoomFactorY = printer->logicalDpiY() / qt_defaultDpi();
@@ -227,7 +221,6 @@ QWebPrinterPrivate::QWebPrinterPrivate(const QWebFrame *f, QPrinter *printer)
 QWebPrinterPrivate::~QWebPrinterPrivate() 
 {
     printContext.end();
-    painter.end();
 }
 
 /*!
@@ -239,8 +232,8 @@ QWebPrinterPrivate::~QWebPrinterPrivate()
 
     \sa QWebFrame
 */
-QWebPrinter::QWebPrinter(const QWebFrame *frame, QPrinter *printer)
-    : d(new QWebPrinterPrivate(frame, printer))
+QWebPrinter::QWebPrinter(const QWebFrame *frame, QPrinter *printer, QPainter &painter)
+    : d(new QWebPrinterPrivate(frame, printer, painter))
 {}
 
 QWebPrinter::~QWebPrinter() 
@@ -1343,7 +1336,9 @@ bool QWebFrame::event(QEvent *e)
 */
 void QWebFrame::print(QPrinter *printer) const
 {
-    QWebPrinter p(this, printer);
+    QPainter painter;
+    painter.begin(printer);
+    QWebPrinter p(this, printer, painter);
     int docCopies;
     int pageCopies;
     if (printer->collateCopies()) {
@@ -1399,6 +1394,7 @@ void QWebFrame::print(QPrinter *printer) const
         if ( i < docCopies - 1)
             printer->newPage();
     }
+    painter.end();
 }
 #endif // QT_NO_PRINTER
 
